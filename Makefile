e025e4f2faa54704ccec04a65ec10516d8e0aec9
Muller Lee <s094392@gmail.com>
2022-01-27 23:25:48 +0800

    REDIS

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all:
-	clang++ main.cpp -I/usr/include/tensorflow -ltensorflow_cc -ltensorflow_framework
+	clang++ main.cpp -I/usr/include/tensorflow -ltensorflow_cc -ltensorflow_framework -lhiredis
 run: all
 	./a.out
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <hiredis/hiredis.h>
 #include <stdio.h>
 #include <tensorflow/cc/client/client_session.h>
 #include <tensorflow/cc/ops/standard_ops.h>
@@ -11,19 +12,36 @@ using namespace tensorflow::ops;
 
 int main() {
     Scope root = Scope::NewRootScope();
-
-    int in_channels = 1, out_channels = 1, filter_size = 2;
-    TensorShape sp({filter_size, filter_size, in_channels, out_channels});
     ClientSession session(root);
-    auto input = Variable(root.WithOpName("I"), sp, DT_FLOAT);
-    std::vector<Tensor> outputs;
-    auto assignedI =
-        Assign(root.WithOpName("I_assign"), input,
-               XavierInit(root, in_channels, out_channels, filter_size));
-    TF_CHECK_OK(session.Run({}, {assignedI}, &outputs));
-    Conv conv(root, filter_size, in_channels, out_channels);
-    conv.forward(session, input);
-    Conv conv2(root, filter_size, in_channels, out_channels);
-    conv2.forward(session, input);
+
+    redisContext *c = redisConnect("172.17.0.3", 6379);
+    if (c == NULL || c->err) {
+        if (c) {
+            printf("Error: %s\n", c->errstr);
+            // handle error
+        } else {
+            printf("Can't allocate redis context\n");
+        }
+    }
+    while (true) {
+        string result;
+        redisReply *reply;
+        while (true) {
+            reply = (redisReply *)redisCommand(c, "LPOP foo");
+            if (reply->type != REDIS_REPLY_NIL) break;
+        }
+        result = reply->str;
+
+        int in_channels = 1, out_channels = atoi(reply->str), filter_size = 2;
+        TensorShape sp({filter_size, filter_size, in_channels, out_channels});
+        auto input = Variable(root.WithOpName("I"), sp, DT_FLOAT);
+        std::vector<Tensor> outputs;
+        auto assignedI =
+            Assign(root.WithOpName("I_assign"), input,
+                   XavierInit(root, in_channels, out_channels, filter_size));
+        TF_CHECK_OK(session.Run({}, {assignedI}, &outputs));
+        Conv conv(root, filter_size, in_channels, out_channels);
+        conv.forward(session, input);
+    }
     return 0;
 }
