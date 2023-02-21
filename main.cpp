bc0309d366c108c1fe2b699a5eb0f6c80b4906a3
Muller Lee <s094392@gmail.com>
2023-02-21 19:38:59 +0000

    Dequeue

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include "layer.hpp"
 #include "tensorflow/core/platform/status.h"
 #include "utils.hpp"
+
 using namespace tensorflow;
 using namespace tensorflow::ops;
 
@@ -20,7 +21,9 @@ int main() {
   LOGs("Process start");
 
   Scope root = Scope::NewRootScope();
-  ClientSession session(root);
+  SessionOptions config;
+  config.config.mutable_gpu_options()->set_allow_growth(true);
+  ClientSession session(root, config);
 
   redisContext *c = redisConnect(getenv("REDIS"), 6379);
 
@@ -33,6 +36,12 @@ int main() {
       LOGs("Can't allocate redis context");
     }
   }
+
+  FIFOQueue::Attrs fifoqueue_attr;
+  FIFOQueue queue(root, {DT_FLOAT}, fifoqueue_attr.SharedName("test"));
+  QueueEnqueue enqueue(root.WithOpName("enqueue"), queue, {Output()});
+  QueueDequeue dequeue(root.WithOpName("dequeue"), queue, {DT_FLOAT});
+  QueueSize size(root.WithOpName("size"), queue);
 
   std::vector<Tensor> outputs;
   TensorShape sp({1, 224, 224, 3});
@@ -106,12 +115,34 @@ int main() {
     std::istringstream iss(result);
     freeReplyObject(reply);
 
-    int variable_id, layer_id;
-    iss >> variable_id >> layer_id;
-
-    variables[variable_id] =
-        Vgg16[layer_id]->forward(session, variables[variable_id]);
-    LOGs("Forwarded: Vgg16-", layer_id);
+    string cmd;
+    iss >> cmd;
+    if (cmd == "forward") {
+      int variable_id, layer_id;
+      iss >> variable_id >> layer_id;
+      variables[variable_id] =
+          Vgg16[layer_id]->forward(session, variables[variable_id]);
+      LOGs("Forwarded: Vgg16-", layer_id);
+    } else if (cmd == "push") {
+      int variable_id;
+      iss >> variable_id;
+      std::vector<Tensor> outputs;
+      TF_CHECK_OK(
+          session.Run({}, {variables[variable_id]}, {enqueue}, &outputs));
+      TF_CHECK_OK(session.Run({}, {size}, {}, &outputs));
+      int value_3 = *outputs[0].scalar<int>().data();
+      LOGs("Enqueued ", variable_id, "size: ", value_3);
+    } else if (cmd == "pop") {
+      int variable_id;
+      iss >> variable_id;
+      LOGs("POP", variable_id);
+      std::vector<Tensor> outputs;
+      TF_CHECK_OK(session.Run({}, {variables[variable_id]}, {dequeue.operation},
+                              &outputs));
+      TF_CHECK_OK(session.Run({}, {size}, {}, &outputs));
+      int value_3 = *outputs[0].scalar<int>().data();
+      LOGs("Dequeued ", variable_id, "size: ", value_3);
+    }
   }
   return 0;
 }
